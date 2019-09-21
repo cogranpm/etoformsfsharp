cf7003ec56db9dc8a78acf792d6d46d2cdaf9c87
paul martin <mussym@live.com>
2019-09-21 11:56:45 -0500

    tying out eto forms models

---
+++ etofsharp/Model.fs
@@ -0,0 +1,15 @@
+﻿(* attempting to implement EtoForms databinding *)
+namespace model
+
+module models =
+
+    open System
+    open System.ComponentModel
+
+    type MyModel() =
+
+        let ev = new Event<_,_>()
+
+        interface INotifyPropertyChanged with
+            [<CLIEvent>]
+            member x.PropertyChanged = ev.Publish
